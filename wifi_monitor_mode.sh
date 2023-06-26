d6662313092f19d98020a0ea730f2d8172e8191d
subhajitsikder18 <subhajitsikder18@gmail.com>
2023-06-27 00:44:11 +0530

    wifi monitor tool

---
+++ wifi_monitor_mode.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+lsusb
+echo "PLEASE CONNECT YOUR ALFA  NETWORK DEVICE & IT WILL AUTOMATACALLY CONNECTED AFTER GETTING SIGNAL"
+sleep 19
+iwconfig
+airmon-ng check kill
+iwconfig wlan0 mode Monitor
+iwconfig
+aireplay-ng --test wlan0
+#iwconfig wlan0 mode Managed
