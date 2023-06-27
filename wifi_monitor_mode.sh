a4b31cf53b6c3cdcd248718ce462bfaf02193f4f
subhajitsikder18 <137827477+subhajitsikder18@users.noreply.github.com>
2023-06-27 23:17:47 +0530

    Update wifi_monitor_mode.sh

--- wifi_monitor_mode.sh
+++ wifi_monitor_mode.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
+echo "PLEASE CONNECT YOUR ALFA  NETWORK DEVICE & IT WILL AUTOMATICALLY CONNECTED AFTER GETTING SIGNAL"
 lsusb
-echo "PLEASE CONNECT YOUR ALFA  NETWORK DEVICE & IT WILL AUTOMATACALLY CONNECTED AFTER GETTING SIGNAL"
 sleep 19
 iwconfig
 airmon-ng check kill
 iwconfig wlan0 mode Monitor
 iwconfig
 aireplay-ng --test wlan0
-#iwconfig wlan0 mode Managed
